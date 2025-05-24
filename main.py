73b53a9fe192847a08a3e3502b8060c3018f277c
Kkoder27 <brunswickkm7@gmail.com>
2025-05-24 17:45:35 -0400

    initial comit

---
+++ main.py
@@ -0,0 +1,4 @@
+"""This is a snippet of python code that serves to satisfy the touchstone requirements of
+CS1100 (Intro to python) from Sophia Learning LLC."""
+# See https://github.com/Kkoder27/SophiaPythonTouchstone for more information
+
