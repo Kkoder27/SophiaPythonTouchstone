50d03a27896df33780d113f58d0531c795a9a97b
Kkoder27 <brunswickkm7@gmail.com>
2025-06-02 11:29:47 -0400

    initial encoding of cypher information

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 CS1100 (Intro to python) from Sophia Learning LLC."""
 # See https://github.com/Kkoder27/SophiaPythonTouchstone for more information
 
+def caesarSolve():
+    caesarInput = input('Input encrypted string to be solved')
+    encryptedSnapshot = list(caesarInput)
+    encodedLetterCase = []
+    for letter in encryptedSnapshot:
+        if ord(letter)>=65 and ord(letter)<=90:
+            encodedLetterCase.append(True)
+        elif ord(letter)>=97 and ord(letter)<=122:
+            encodedLetterCase.append(False)
+        else: encodedLetterCase.append(letter)
+    print(encryptedSnapshot)
+    print(encodedLetterCase)
+    return
+caesarSolve()
