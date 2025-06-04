b86f40f9a386da601628f1a0b00be96634294629
Kkoder27 <brunswickkm7@gmail.com>
2025-06-04 12:46:10 -0400

    completed english probability and output for caeser shifted solutions

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 CS1100 (Intro to python) from Sophia Learning LLC."""
 # See https://github.com/Kkoder27/SophiaPythonTouchstone for more information
 
-import re
+import math
+from english_words import get_english_words_set as gew #provides set of bulk english words
+englishSet = gew(['web2'], lower=True)
 
 def encodeCaseInfo(caesarInput):
     encryptedSnapshot = list(caesarInput) #string to list will be used to work with ASCII information
@@ -16,7 +18,7 @@ def encodeCaseInfo(caesarInput):
         else: encodedLetterCase.append(letter) #maintains current symbol if not english alphabet, use to catch errors later
     return encodedLetterCase
 
-def loopCaesarValues(shiftValue, digit):
+def loopCaesarValues(shiftValue, digit): #input letter & shift value, output new letter
     while shiftValue>25:
         shiftValue-=26
     output = digit + shiftValue
@@ -42,14 +44,46 @@ def bruteForceShift(caesarInput): #takes string, and turns into list of strings 
     for arr in range(len(outputArr)):
         outputArr[arr] = ''.join(outputArr[arr])
     return outputArr
-    
 
+def cutNonAlphas(string): #cuts out nonalphabeticals for english comparison
+    expandedList = list(string)
+    step=0
+    outputString = ''
+    for item in expandedList:
+        if ord(item)>=97 and ord(item)<=122 or ord(item)==32: #catches lowercase letters or a space
+            outputString = outputString + expandedList[step]
+        step+=1
+    #print(outputString)
+    return outputString
+
+def englishComparison(manyList):
+    probabilityList = []
+    for cipherOption in manyList:
+        accurateWords = 0
+        cipherWords = cutNonAlphas(cipherOption).split(' ')
+        print(cipherWords)
+        for word in cipherWords:
+            if word in englishSet:
+                accurateWords+=1
+        probability = math.trunc((accurateWords/len(cipherWords))*100)
+        probabilityList.append(probability)
+    cipherNum, steps, maxProb = 0,0,0
+    for probability in probabilityList:
+        if probability > maxProb:
+            maxProb = probability
+            cipherNum = steps
+        steps+=1
+        #print([cipherNum, maxProb])
+    #print(probabilityList)
+    return cipherNum
+    
 def caesarSolve():
     caesarInput = input('Input encrypted string to be solved')
     encodedLetterCase = encodeCaseInfo(caesarInput) #encoded information about case of letters
     manyList = bruteForceShift(caesarInput) #list of strings corresponding to each possible caeser shift
-    
+    cipherNum = englishComparison(manyList)
+    #print(manyList[cipherNum])
     #print(encodedLetterCase)
-    print(manyList)
-    return
+    #print(manyList)
+    return manyList[cipherNum]
 caesarSolve()
